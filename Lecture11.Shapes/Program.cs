ebd5359640d8fe60a1c7e3ad9be5fb45bcc587e2
Unknown <jobbazjob@gmail.com>
2020-04-14 22:00:58 +0300

    Lect_11

---
+++ Lecture11.Shapes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lecture11.Shapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Lecture11.Shapes/Shape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lecture11.Shapes
+{
+    class Shape
+    {
+        public int side = 2;
+        public int S;
+
+      public virtual void Area() 
+        {
+            S = side ^ 2;
+        }
+    }
+}
