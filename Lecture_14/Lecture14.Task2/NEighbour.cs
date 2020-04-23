96dfacd08bd97050d120f6d132ac5ed4827a8121
Unknown <jobbazjob@gmail.com>
2020-04-23 20:09:50 +0300

    Lect_14

---
+++ Lecture_14/Lecture14.Task2/NEighbour.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lecture14.Task2
+{
+    class NEighbour
+    {
+        public string FullName { get; set; }
+        public int Flatnumber { get; set; }
+        public int PhoneNumber { get; set;  }
+    }
+}
---
+++ Lecture_14/Lecture_14/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lecture_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Hello, guys!!!!";
+            var nameIndex = s1.IndexOf(',');
+            s1 = s1.Remove(nameIndex, 10);
+            Console.WriteLine(s1 + "...");
+        }
+
+        static void StringBuild() 
+        {
+            
+
+        }
+    }
+}
